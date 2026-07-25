60667dac9c3ec703264bc2fa0eb989a5a03edcee
Tejas S <tejasvijay96@gmail.com>
2026-07-25 19:04:21 +0530

    Refactored the code in app

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,22 @@
 import { Hero } from "@/sections/Hero";
-import { FeatureShowcase } from "@/sections/FeatureShowcase";
-import { FeatureCards } from "@/sections/FeatureCards";
+// import { FeatureShowcase } from "@/sections/FeatureShowcase";
 import { LiveInfrastructure } from "@/sections/LiveInfrastructure";
 import { CTASection } from "@/sections/CTASection";
-import { Footer } from "@/sections/Footer";
 import { TrustedBy } from "@/sections/TrustedBy";
 import { TRUSTED_COMPANIES } from "@/lib/constants";
+import { PremiumServices } from "@/sections/PremiumServices";
+import { QualityDrive } from "@/sections/QualityDrive"; 
 
 export default function Home() {
   return (
     <main className="relative overflow-hidden">
       <Hero />
-      <TrustedBy  logos={TRUSTED_COMPANIES} />    
-      <FeatureShowcase />
-      <FeatureCards />
+      <TrustedBy logos={TRUSTED_COMPANIES} />  
+      <QualityDrive /> 
+      <PremiumServices /> 
+      {/* <FeatureShowcase /> */}
       <LiveInfrastructure />
       <CTASection />
-      <Footer />
     </main>
   );
 }
