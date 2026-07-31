238223faab3c4d228aeec57377a0538d97eed623
Tejas S <tejasvijay96@gmail.com>
2026-07-31 20:22:25 +0530

    Added new brands

--- components/data/fireProtection.ts
+++ components/data/fireProtection.ts
@@ -1,10 +1,8 @@
-export const fireProtectionBrands = [
+const brands = [
   { name: "Honeywell", logo: "/images/honeywell.png" },
   { name: "Siemens", logo: "/images/siemens.png" },
-  { name: "Johnson Controls", logo: "/images/JohnsonControls.png" },
+  { name: "Notifier", logo: "/images/notifier.png" },
   { name: "Tyco", logo: "/images/tyco.png" },
-  { name: " Simplex", logo: "/images/simplex.png" },
+  { name: "Simplex", logo: "/images/simplex.png" },
+  {name:"JohnsonControls",logo:"images/JohnsonControls.png"}
 ];
-
-
-
