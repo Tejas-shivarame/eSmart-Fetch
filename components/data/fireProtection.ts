3a772f19c349c335e3b545893c42b28bb43c3480
Tejas S <tejasvijay96@gmail.com>
2026-07-31 12:22:54 +0530

    added data file for fireprtection

---
+++ components/data/fireProtection.ts
@@ -0,0 +1,10 @@
+export const fireProtectionBrands = [
+  { name: "Honeywell", logo: "/images/honeywell.png" },
+  { name: "Siemens", logo: "/images/siemens.png" },
+  { name: "Johnson Controls", logo: "/images/JohnsonControls.png" },
+  { name: "Tyco", logo: "/images/tyco.png" },
+  { name: " Simplex", logo: "/images/simplex.png" },
+];
+
+
+
