a177f4d3cfb1009cb09194a5feb11e4c7a6513d1
Tejas S <tejasvijay96@gmail.com>
2026-08-01 20:20:25 +0530

    Remove unused files

--- components/data/fireProtection.ts
+++
@@ -1,8 +0,0 @@
-const brands = [
-  { name: "Honeywell", logo: "/images/honeywell.png" },
-  { name: "Siemens", logo: "/images/siemens.png" },
-  { name: "Notifier", logo: "/images/notifier.png" },
-  { name: "Tyco", logo: "/images/tyco.png" },
-  { name: "Simplex", logo: "/images/simplex.png" },
-  {name:"JohnsonControls",logo:"images/JohnsonControls.png"}
-];
