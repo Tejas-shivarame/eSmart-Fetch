71fcdb0e30b4acffdd1a2329cb0683e9b38d09ef
Tejas S <tejasvijay96@gmail.com>
2026-08-03 01:20:27 +0530

    Render HomeConatctCTA compont

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,20 @@
 import { Hero } from "@/sections/Hero";
-import { TrustedBy } from "@/sections/TrustedBy";
-import { TRUSTED_COMPANIES } from "@/lib/constants";
-import { PremiumServices } from "@/sections/PremiumServices";
-import { QualityDrive } from "@/sections/QualityDrive"; 
 import { AboutSection } from "@/sections/about/AboutSection";
+import { QualityDrive } from "@/sections/QualityDrive";
+import { PremiumServices } from "@/sections/PremiumServices";
+import { TrustedBy } from "@/sections/TrustedBy";
+import { HomeContactCTA } from "@/sections/HomeContactCTA";
+import { TRUSTED_COMPANIES } from "@/lib/constants";
 
 export default function Home() {
   return (
     <main className="relative overflow-hidden">
       <Hero />
-      <AboutSection /> 
-      <QualityDrive />       
-      <PremiumServices /> 
-      <TrustedBy logos={TRUSTED_COMPANIES} /> 
+      <AboutSection />
+      <QualityDrive />
+      <PremiumServices />
+      <TrustedBy logos={TRUSTED_COMPANIES} />
+      <HomeContactCTA /> 
     </main>
   );
 }
